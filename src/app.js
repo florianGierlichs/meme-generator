16a6bc5b9881d1f23cbc6add38caa03b51db2987
florianGierlichs <gierlichs@gmx.de>
2020-03-08 01:44:31 +0100

    Integrate API

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './app.scss';
 import { createElement, appendContent } from './lib/dom';
-//import { generateRandomMeme } from './components/api';
+import { generateRandomMeme } from './components/api';
 
 export function app() {
   const main = createElement('main', {
@@ -9,7 +9,24 @@ export function app() {
   const startButton = createElement('button', {
     className: 'startButton'
   });
-  appendContent(main, startButton);
+
+  const wrapper = createElement('div', {
+    className: 'wrapper'
+  });
+
+  appendContent(main, [startButton, wrapper]);
+
+  startButton.addEventListener('click', async () => {
+    wrapper.innerHTML = '';
+    const randomMeme = await generateRandomMeme();
+
+    const memePicture = await createElement('img', {
+      className: 'memePicture',
+      src: randomMeme
+    });
+
+    appendContent(wrapper, memePicture);
+  });
 
   return [main];
 }
